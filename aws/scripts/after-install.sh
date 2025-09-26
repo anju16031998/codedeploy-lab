e447b4939ad1c2e4d5f2a3ec205a0c269e3fb3c2
anju16031998 <anju16031998@gmail.com>
2025-09-25 22:01:24 -0400

    fix: Updated S3 bucket handling in GitHub Actions and deployment scripts

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -xe
 
+# Get the S3 bucket name from the CloudFormation stack outputs
+S3_BUCKET=$(aws cloudformation describe-stacks --stack-name codedeploy-lab --query 'Stacks[0].Outputs[?OutputKey==`DeploymentBucket`].OutputValue' --output text)
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://alabstack-webappdeploymentbucket-8kefukl7oav9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://${S3_BUCKET}/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
